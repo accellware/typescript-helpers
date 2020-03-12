21d2713af4f4e05f1b26e8d4306b566b6996595d
Nabil <nblx2k4@users.noreply.github.com>
2020-03-12 11:29:09 +0100

    Create objectMap.ts
    object map function to dynamically map object attributes

---
+++ helpers/objectMap.ts
@@ -0,0 +1,13 @@
+export function objectMap<TSource, TDestination>(source: TSource, destType: new () => TDestination): TDestination {
+    if (source == null) {
+        return null;
+    }
+
+    const destination = new destType();
+
+    Object.keys(source).forEach((key) => {
+        if (key in destination) {
+            (destination as any)[key] = (source as any)[key];
+        }
+    });
+}
